3be15099bc8cdee4208839eda92d69c8a7835421
Darryl Wright <darryl@hey.com>
2023-03-16 11:18:45 -0300

    Initial commit.

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/punkscience/titlefy
+
+go 1.18
+
+require golang.org/x/text v0.8.0
---
+++ titlefy.go
@@ -0,0 +1,45 @@
+package titlefy
+
+import (
+	"strings"
+
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
+)
+
+func formatEnglishTitle(str string) string {
+	conjunctions := []string{
+		"for",
+		"and",
+		"nor",
+		"but",
+		"or",
+		"yet",
+		"so",
+	}
+
+	output := ""
+
+	// Now break up the string
+	mots := strings.Split(str, " ")
+
+	for i, mot := range mots {
+
+		// Skip the first token or anything larger than 3 letters
+		if i == 0 || len(mot) > 3 {
+			output += mot + " "
+			continue
+		}
+
+		// Look for the ones we want to shift
+		for _, conjunction := range conjunctions {
+			if strings.EqualFold(mot, conjunction) {
+				output += cases.Title(language.English).String(conjunction) + " "
+				break
+			}
+		}
+	}
+
+	return strings.Trim(output, " ")
+
+}
